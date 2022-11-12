d7a02f7f911852cc14abe807f837896239b7d018
DaniGM17 <daniagm75@gmail.com>
2022-11-11 20:43:45 -0600

    Inicio de proyecto, creación clase Nodo

---
+++ src/ListaReproduccion/Nodo.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ListaReproduccion;
+
+/**
+ *
+ * @author dania
+ */
+public class Nodo<T> {
+
+    private T objeto;//dato almacendo
+    private Nodo siguiente;// apuntador al siguiente nodo, referencia a la misma clase
+
+    public Nodo(T o) {
+        this.objeto = o;
+        this.siguiente = null;
+    }
+
+    public T getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(T o) {
+        this.objeto = o;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo otroNodo) {
+        this.siguiente = otroNodo;
+    }
+
+   
+
+}
