105ab2977f6cfa830389d207a8c1d2f3294d9467
DaniGM17 <daniagm75@gmail.com>
2022-11-12 10:19:40 -0600

    Se implementa el menú, y se pruebas las opciones de agregar e imprimir

--- src/ListaReproduccion/TestingPlaylist.java
+++ src/ListaReproduccion/TestingPlaylist.java
@@ -15,75 +15,107 @@ import java.io.InputStreamReader;
 public class TestingPlaylist {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
-        String entrada;
         char respuesta;
         Lista lista = new Lista();
+        String usuario;
+        int opcion;
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String entrada;
+        char continuar;
 
-        for (;;) {//ciclo infinito
-            System.out.println("Nombre: ");
-            entrada = bufer.readLine();
-            String nombre = entrada;
-            System.out.println("Interpetre: ");
-            entrada = bufer.readLine();
-            String interpetre = entrada;
-            System.out.println("Album: ");
-            entrada = bufer.readLine();
-            String album = entrada;
-            System.out.println("Duracion: ");
-            entrada = bufer.readLine();
-            double duracion = Double.parseDouble(entrada);
-            System.out.println("Genero: ");
-            entrada = bufer.readLine();
-            String genero = entrada;
-            System.out.println("Favorito: ");
-            entrada = bufer.readLine();
-            boolean favoritos = Boolean.parseBoolean(entrada);
-            System.out.println("Año: ");
-            entrada = bufer.readLine();
-            int year = Integer.parseInt(entrada);
-            Cancion unaCancion = new Cancion(nombre, interpetre, album, duracion, genero, favoritos, year);
-            lista.add(unaCancion);
-            System.out.println("¿Se ha agregado la canción a la lista?");
-            System.out.println("¿Quieres agregar más canciones? (y/n)");
-            entrada = bufer.readLine();
-            respuesta = entrada.charAt(0);
-            if (respuesta == 'n' || respuesta == 'N') {
+        System.out.println("Ingresa tu nombre");
+        entrada = bufer.readLine();
+        usuario = entrada;
+
+        System.out.println("Bienvenid@ " + usuario + " a tu top de canciones");
+        System.out.println("¿Qué quieres hacer hoy?");
+        do{
+        System.out.println("1.-Agregar una canción");
+        System.out.println("2.-Agregar una canción a una posición ya utilizada");
+        System.out.println("3.-Borrar la última canción agregada");
+        System.out.println("4.-Borrar una canción de una posición");
+        System.out.println("5.-Buscar canción por posición");
+        System.out.println("6.-Buscar canción por nombre");
+        System.out.println("7.-Mostrar mi lista de reproducción");
+        entrada = bufer.readLine();
+        opcion = Integer.parseInt(entrada);
+
+        switch (opcion) {
+            case 1:
+                for (;;) {//ciclo infinito
+                    System.out.println("Nombre: ");
+                    entrada = bufer.readLine();
+                    String nombre = entrada;
+                    System.out.println("Interpetre: ");
+                    entrada = bufer.readLine();
+                    String interpetre = entrada;
+                    System.out.println("Album: ");
+                    entrada = bufer.readLine();
+                    String album = entrada;
+                    System.out.println("Duracion: ");
+                    entrada = bufer.readLine();
+                    double duracion = Double.parseDouble(entrada);
+                    System.out.println("Genero: ");
+                    entrada = bufer.readLine();
+                    String genero = entrada;
+                    System.out.println("Favorito: ");
+                    entrada = bufer.readLine();
+                    boolean favoritos = Boolean.parseBoolean(entrada);
+                    System.out.println("Año: ");
+                    entrada = bufer.readLine();
+                    int year = Integer.parseInt(entrada);
+                    Cancion unaCancion = new Cancion(nombre, interpetre, album, duracion, genero, favoritos, year);
+                    lista.add(unaCancion);
+                    System.out.println("Se ha agregado la canción a la lista");
+                    System.out.println("¿Quieres agregar más canciones? (y/n)");
+                    entrada = bufer.readLine();
+                    respuesta = entrada.charAt(0);
+                    if (respuesta == 'n' || respuesta == 'N') {
+                        break;
+                    }
+                }
                 break;
-            }
+            case 2:
+                System.out.println("------------------------------");
+                System.out.println("Inserción de una nueva canción");
+                System.out.println("Nombre: ");
+                entrada = bufer.readLine();
+                String nombre = entrada;
+                System.out.println("Interpetre: ");
+                entrada = bufer.readLine();
+                String interpetre = entrada;
+                System.out.println("Album: ");
+                entrada = bufer.readLine();
+                String album = entrada;
+                System.out.println("Duracion: ");
+                entrada = bufer.readLine();
+                double duracion = Double.parseDouble(entrada);
+                System.out.println("Genero: ");
+                entrada = bufer.readLine();
+                String genero = entrada;
+                System.out.println("Favorito: ");
+                entrada = bufer.readLine();
+                boolean favoritos = Boolean.parseBoolean(entrada);
+                System.out.println("Año: ");
+                entrada = bufer.readLine();
+                int year = Integer.parseInt(entrada);
+                Cancion unaCancion = new Cancion(nombre, interpetre, album, duracion, genero, favoritos, year);
+                System.out.println("Escriba la posición en donde quieras insertar:");
+                entrada = bufer.readLine();
+                int p = Integer.parseInt(entrada);
+                lista.add(unaCancion, p);
+                break;
+
+            case 7:
+                System.out.println("------------------------------");
+                System.out.println("Contenido de la playlist: ");
+                lista.printList();
+                break;
         }
-
-        System.out.println("------------------------------");
-        System.out.println("Inserción de una nueva canción");
-        System.out.println("Nombre: ");
-        entrada = bufer.readLine();
-        String nombre = entrada;
-        System.out.println("Interpetre: ");
-        entrada = bufer.readLine();
-        String interpetre = entrada;
-        System.out.println("Album: ");
-        entrada = bufer.readLine();
-        String album = entrada;
-        System.out.println("Duracion: ");
-        entrada = bufer.readLine();
-        double duracion = Double.parseDouble(entrada);
-        System.out.println("Genero: ");
-        entrada = bufer.readLine();
-        String genero = entrada;
-        System.out.println("Favorito: ");
-        entrada = bufer.readLine();
-        boolean favoritos = Boolean.parseBoolean(entrada);
-        System.out.println("Año: ");
-        entrada = bufer.readLine();
-        int year = Integer.parseInt(entrada);
-        Cancion unaCancion = new Cancion(nombre, interpetre, album, duracion, genero, favoritos, year);;
-        System.out.println("Escriba la posición en donde quieras insertar:");
-        entrada = bufer.readLine();
-        int p = Integer.parseInt(entrada);
-        lista.add(unaCancion, p);
-
-        System.out.println("------------------------------");
-        System.out.println("Contenido de la playlist: ");
-        lista.printList();
+            System.out.println("¿Quieres realizar otra acción?");
+            entrada = bufer.readLine();
+            continuar=entrada.charAt(0);
+        }while(continuar == 'Y' || continuar == 'y');
+        
     }
 }
