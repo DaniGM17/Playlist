967c5b62123938c2461943d1a17e99c3e3831bf2
DaniGM17 <daniagm75@gmail.com>
2022-11-12 11:31:49 -0600

    Función de agregar al inicio

--- src/ListaReproduccion/Lista.java
+++ src/ListaReproduccion/Lista.java
@@ -9,36 +9,37 @@ package ListaReproduccion;
  * @author dania
  */
 public class Lista<T> {
+
     private Nodo head; //Apuntador a la cabecera de la lista(primer nodo)
     private Nodo tail;//Apuntador a la cola de la lista (último nodo)
     private Nodo actual;
-    private  int size;//Tamaño de la lista(número de nodos en la lista)
+    private int size;//Tamaño de la lista(número de nodos en la lista)
 
-    public Lista(){
-        this. head=null;
-        this.actual=null;
-        this.tail=null;
-        this.size=0;
+    public Lista() {
+        this.head = null;
+        this.actual = null;
+        this.tail = null;
+        this.size = 0;
     }
     
-    public void add(Cancion cancion){
+    public void add(Cancion cancion) {
         Nodo nuevoNodo = new Nodo(cancion);
         this.size++;
         this.tail = nuevoNodo; //se mueve el apuntador al último nodo
-        if(head==null){//La lista está vacía, creamos el nodo
+        if (head == null) {//La lista está vacía, creamos el nodo
             head = nuevoNodo;
             actual = head;
-        }else{
+        } else {
             actual.setSiguiente(nuevoNodo); //actual.sig apunta al nuevoNodo (enlace entre nodos)
             actual = nuevoNodo;//Ahora actual es el nuevo nodo
         }
     }
     
-     public void add(Cancion cancion, int posicion){
+    public void add(Cancion cancion, int posicion) {
         Nodo nuevoNodo = new Nodo(cancion);
         int i = 1;
         actual = head;
-        while (i<posicion - 1){
+        while (i < posicion - 1) {
             actual = actual.getSiguiente();
             i++;
         }
@@ -48,10 +49,18 @@ public class Lista<T> {
         actual.setSiguiente(auxiliar);
     }
     
-    public void printList(){
+    public void addInicio(Cancion cancion) {
+        Nodo nuevoNodo = new Nodo(cancion);
+        Nodo aux = head;
+        head = nuevoNodo;
+        head.setSiguiente(aux);
+        
+    }
+    
+    public void printList() {
         int posicion = 1;
         actual = head;
-        while (actual != null){
+        while (actual != null) {
             System.out.println("Canción " + posicion);
             System.out.println(actual.getObjeto().toString());
             actual = actual.getSiguiente();//obtiene dirección del siguiente nodo para que se pueda mover
--- src/ListaReproduccion/TestingPlaylist.java
+++ src/ListaReproduccion/TestingPlaylist.java
@@ -29,20 +29,56 @@ public class TestingPlaylist {
 
         System.out.println("Bienvenid@ " + usuario + " a tu top de canciones");
         System.out.println("¿Qué quieres hacer hoy?");
-        do{
-        System.out.println("1.-Agregar una canción");
-        System.out.println("2.-Agregar una canción a una posición ya utilizada");
-        System.out.println("3.-Borrar la última canción agregada");
-        System.out.println("4.-Borrar una canción de una posición");
-        System.out.println("5.-Buscar canción por posición");
-        System.out.println("6.-Buscar canción por nombre");
-        System.out.println("7.-Mostrar mi lista de reproducción");
-        entrada = bufer.readLine();
-        opcion = Integer.parseInt(entrada);
+        do {
+            System.out.println("1.-Agregar una canción");
+            System.out.println("2.-Agregar una canción a una posición ya utilizada");
+            System.out.println("3.-Borrar la última canción agregada");
+            System.out.println("4.-Borrar una canción de una posición");
+            System.out.println("5.-Buscar canción por posición");
+            System.out.println("6.-Buscar canción por nombre");
+            System.out.println("7.-Mostrar mi lista de reproducción");
+            System.out.println("8.-¿Cuantás canciones hay en mi lista?");
+            entrada = bufer.readLine();
+            opcion = Integer.parseInt(entrada);
 
-        switch (opcion) {
-            case 1:
-                for (;;) {//ciclo infinito
+            switch (opcion) {
+                case 1:
+                    for (;;) {//ciclo infinito
+                        System.out.println("Nombre: ");
+                        entrada = bufer.readLine();
+                        String nombre = entrada;
+                        System.out.println("Interpetre: ");
+                        entrada = bufer.readLine();
+                        String interpetre = entrada;
+                        System.out.println("Album: ");
+                        entrada = bufer.readLine();
+                        String album = entrada;
+                        System.out.println("Duracion: ");
+                        entrada = bufer.readLine();
+                        double duracion = Double.parseDouble(entrada);
+                        System.out.println("Genero: ");
+                        entrada = bufer.readLine();
+                        String genero = entrada;
+                        System.out.println("Favorito: ");
+                        entrada = bufer.readLine();
+                        boolean favoritos = Boolean.parseBoolean(entrada);
+                        System.out.println("Año: ");
+                        entrada = bufer.readLine();
+                        int year = Integer.parseInt(entrada);
+                        Cancion unaCancion = new Cancion(nombre, interpetre, album, duracion, genero, favoritos, year);
+                        lista.add(unaCancion);
+                        System.out.println("Se ha agregado la canción a la lista");
+                        System.out.println("¿Quieres agregar más canciones? (y/n)");
+                        entrada = bufer.readLine();
+                        respuesta = entrada.charAt(0);
+                        if (respuesta == 'n' || respuesta == 'N') {
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("------------------------------");
+                    System.out.println("Inserción de una nueva canción");
                     System.out.println("Nombre: ");
                     entrada = bufer.readLine();
                     String nombre = entrada;
@@ -65,57 +101,26 @@ public class TestingPlaylist {
                     entrada = bufer.readLine();
                     int year = Integer.parseInt(entrada);
                     Cancion unaCancion = new Cancion(nombre, interpetre, album, duracion, genero, favoritos, year);
-                    lista.add(unaCancion);
-                    System.out.println("Se ha agregado la canción a la lista");
-                    System.out.println("¿Quieres agregar más canciones? (y/n)");
+                    System.out.println("Escriba la posición en donde quieras insertar:");
                     entrada = bufer.readLine();
-                    respuesta = entrada.charAt(0);
-                    if (respuesta == 'n' || respuesta == 'N') {
-                        break;
+                    int p = Integer.parseInt(entrada);
+                    if (p == 1) {
+                        lista.addInicio(unaCancion);
+                    } else {
+                        lista.add(unaCancion, p);
                     }
-                }
-                break;
-            case 2:
-                System.out.println("------------------------------");
-                System.out.println("Inserción de una nueva canción");
-                System.out.println("Nombre: ");
-                entrada = bufer.readLine();
-                String nombre = entrada;
-                System.out.println("Interpetre: ");
-                entrada = bufer.readLine();
-                String interpetre = entrada;
-                System.out.println("Album: ");
-                entrada = bufer.readLine();
-                String album = entrada;
-                System.out.println("Duracion: ");
-                entrada = bufer.readLine();
-                double duracion = Double.parseDouble(entrada);
-                System.out.println("Genero: ");
-                entrada = bufer.readLine();
-                String genero = entrada;
-                System.out.println("Favorito: ");
-                entrada = bufer.readLine();
-                boolean favoritos = Boolean.parseBoolean(entrada);
-                System.out.println("Año: ");
-                entrada = bufer.readLine();
-                int year = Integer.parseInt(entrada);
-                Cancion unaCancion = new Cancion(nombre, interpetre, album, duracion, genero, favoritos, year);
-                System.out.println("Escriba la posición en donde quieras insertar:");
-                entrada = bufer.readLine();
-                int p = Integer.parseInt(entrada);
-                lista.add(unaCancion, p);
-                break;
+                    break;
 
-            case 7:
-                System.out.println("------------------------------");
-                System.out.println("Contenido de la playlist: ");
-                lista.printList();
-                break;
-        }
+                case 7:
+                    System.out.println("------------------------------");
+                    System.out.println("Contenido de la playlist: ");
+                    lista.printList();
+                    break;
+            }
             System.out.println("¿Quieres realizar otra acción?");
             entrada = bufer.readLine();
-            continuar=entrada.charAt(0);
-        }while(continuar == 'Y' || continuar == 'y');
-        
+            continuar = entrada.charAt(0);
+        } while (continuar == 'Y' || continuar == 'y');
+
     }
 }
